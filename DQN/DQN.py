0c2688afdbfe8e78b8dd8ab1f86c861625dc59c5
Jycc <2797824480@qq.com>
2026-07-27 22:48:08 +0800

    CartPole-v1模型以及运行代码修改

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -2,6 +2,6 @@ import xuance
 runner = xuance.get_runner('dqn',
                        'classic_control',  # 可选：classic_control、box2d、atari。
                        'CartPole-v1',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
-                       "DQN_CartPole-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
+                       "./DQN/DQN_CartPole-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
                        )
 runner.run(mode='benchmark')  # 或使用 runner.benchmark()
--- DuelingDQN/DuelingDQN.py
+++ DuelingDQN/DuelingDQN.py
@@ -2,6 +2,6 @@ import xuance
 runner = xuance.get_runner('dueldqn',
                        'classic_control',  # 可选：classic_control、box2d、atari。
                        'CartPole-v1',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
-                       "DuelingDQN_CartPole-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
+                       "./DuelingDQN/DuelingDQN_CartPole-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
                        )
 runner.run(mode='benchmark')
