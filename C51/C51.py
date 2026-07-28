c1a7cacfcc5d44448541f2436e5ceb4d4a93b3a0
Jycc <2797824480@qq.com>
2026-07-28 20:16:46 +0800

    MountainCar-v0模型

--- C51/C51.py
+++ C51/C51.py
@@ -1,7 +1,7 @@
 import xuance
 runner = xuance.get_runner('c51',
                        'classic_control',  # 可选：classic_control、box2d、atari。
-                       'Acrobot-v1',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
-                       "./C51/C51_Acrobot-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
+                       'MountainCar-v0',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
+                       "./C51/C51_MountainCar-v0.yaml",  # 请确保 my_config.yaml 的路径正确。
                        )
 runner.run(mode='benchmark') 
--- DuelingDQN/DuelingDQN.py
+++ DuelingDQN/DuelingDQN.py
@@ -1,7 +1,7 @@
 import xuance
 runner = xuance.get_runner('dueldqn',
                        'classic_control',  # 可选：classic_control、box2d、atari。
-                       'Acrobot-v1',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
-                       "./DuelingDQN/DuelingDQN_Acrobot-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
+                       'MountainCar-v0',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
+                       "./DuelingDQN/DuelingDQN_MountainCar-v0.yaml",  # 请确保 my_config.yaml 的路径正确。
                        )
 runner.run(mode='benchmark')
