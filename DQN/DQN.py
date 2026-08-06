f354f671b8a78474967685626fcac4cd745b1d23
Jycc <2797824480@qq.com>
2026-08-06 10:24:38 +0800

    重新运行之前失败的和效果不好的模型

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -1,7 +1,7 @@
 import xuance
 runner = xuance.get_runner('dqn',
                        'classic_control',  # 可选：classic_control、box2d、atari。
-                       'MountainCar-v0',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
-                       "./DQN/DQN_MountainCar-v0.yaml",  # 请确保 my_config.yaml 的路径正确。
+                       'CartPole-v1',  # 可选：CartPole-v1、Acrobot-v1、MountainCar-v0 等。
+                       "./DQN/DQN_CartPole-v1.yaml",  # 请确保 my_config.yaml 的路径正确。
                        )
 runner.run(mode='benchmark')  # 或使用 runner.benchmark()
